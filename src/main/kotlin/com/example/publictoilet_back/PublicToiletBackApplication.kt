9db9e57f1b4f8fa82b74e5501eba235ffd5d59af
SangwonYoon <yoonsw0532@naver.com>
2022-06-24 12:54:41 +0900

    initial commit

---
+++ src/main/kotlin/com/example/publictoilet_back/PublicToiletBackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.publictoilet_back
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PublicToiletBackApplication
+
+fun main(args: Array<String>) {
+	runApplication<PublicToiletBackApplication>(*args)
+}

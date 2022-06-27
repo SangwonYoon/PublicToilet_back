5d6509729658b16357408c0482ce5f6829ab9466
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2022-06-27 19:12:29 +0900

    근처 화장실 위치 검색 URL 경로 수정

--- src/main/kotlin/com/example/publictoilet_back/web/ToiletApiController.kt
+++ src/main/kotlin/com/example/publictoilet_back/web/ToiletApiController.kt
@@ -5,18 +5,19 @@ import com.example.publictoilet_back.service.toilet.ToiletService
 import com.example.publictoilet_back.web.dto.ToiletResponseDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ToiletApiController(val toiletService: ToiletService) {
 
-    @GetMapping("/api/v1/toilet/{id}")
+    @GetMapping("/api/v1/toilets/{id}")
     fun findById(@PathVariable id : Long) : ToiletResponseDto{
         return toiletService.findById(id)
     }
 
-    @GetMapping("api/v1/toilet/{temp_latitude}/{temp_longitude}/{range}")
-    fun findNearToilet(@PathVariable(value = "temp_latitude") latitude : Double, @PathVariable(value = "temp_longitude") longitude : Double, @PathVariable(value = "range") range : Int) : MutableList<ToiletResponseDto>{
+    @GetMapping("api/v1/toilets")
+    fun findNearToilet(@RequestParam latitude : Double, @RequestParam longitude : Double, @RequestParam range : Int) : MutableList<ToiletResponseDto>{
         return toiletService.findNearToilet(latitude, longitude, range)
     }
 }

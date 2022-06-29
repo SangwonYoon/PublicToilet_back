2e507599f6ae6240a94cf5c983003edb6bb794e6
SangwonYoon <38793142+SangwonYoon@users.noreply.github.com>
2022-06-29 14:55:30 +0900

    id 값으로 info 조회하는 API 추가

--- src/main/kotlin/com/example/publictoilet_back/web/controller/InfoApiController.kt
+++ src/main/kotlin/com/example/publictoilet_back/web/controller/InfoApiController.kt
@@ -1,4 +1,16 @@
 package com.example.publictoilet_back.web.controller
 
-class InfoApiController {
+import com.example.publictoilet_back.service.info.InfoService
+import com.example.publictoilet_back.web.dto.InfoResponseDto
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class InfoApiController(val infoService: InfoService) {
+
+    @GetMapping("Info/{id}")
+    fun findById(@PathVariable id : Long) : InfoResponseDto{
+        return infoService.findById(id)
+    }
 }
